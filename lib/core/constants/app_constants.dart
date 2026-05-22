85ad0a11a604ff3550903a0d764bf8be05b344a6
fitale353 <fitale353@gmail.com>
2026-05-23 01:51:35 +0300

    Add AppConstants class with application constants

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,7 @@
+class AppConstants {
+  static const String appName = 'Home Tweak';
+  static const String baseUrl = 'https://api.hometweak.com/v1';
+  
+  // Design paddings
+  static const double defaultPadding = 16.0;
+}
